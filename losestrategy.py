4526098ef87a9ed5d6203d5510da1e81dc51e842
Madeleine Gignoux <madeleine.gignoux@gmail.com>
2024-06-20 15:10:37 +0200

    Add strategies

---
+++ losestrategy.py
@@ -0,0 +1,12 @@
+import louiswork
+import simulator
+import nano
+
+def lose_strategy(grid, state):
+    probabilities = 0
+    cells = louiswork.available_cells(state)
+    for cell in cells:
+        probwin = query(louiswork.apply(state, cell, "x"), grid, lose)
+        probabilities.append(cell, probwin) 
+    return cells(probabilities.index(min(probabilities)))
+
---
+++ winstrategy.py
@@ -0,0 +1,12 @@
+import louiswork
+import simulator
+import nano
+
+def win_strategy(grid, state):
+    probabilities = 0
+    cells = louiswork.available_cells(state)
+    for cell in cells:
+        probwin = query(louiswork.apply(state, cell, "x"), grid, win)
+        probabilities.append(cell, probwin) 
+    return cells(probabilities.index(max(probabilities)))
+
