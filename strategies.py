f7f93ed6030077d4672bce98c9ae7a52613d580f
DdosSantosGomes <djanira.westra@gmail.com>
2024-06-20 14:44:14 +0200

    Progress strategies

--- strategies.py
+++ strategies.py
@@ -5,47 +5,76 @@ import game_logic
 # 1/9::pos(1,1); 1/9::pos(2,1); 1/9::pos(3,1); 1/9::pos(4,1); 1/9::pos(5,1); 
 #                1/9::pos(6,1); 1/9::pos(7,1); 1/9::pos(8,1); 1/9::pos(9,1).
 
-# Uniform distribution containing all available moves
-def stringForUniDist(state, turn_nr):
-    cells = louiswork.available_cells(state)
-    total = len(cells)
+# Returns Problog-style string of unif. prob. dist. given list of available cell nrs
+def stringOfProbDist(av_cell_nrs, turn_nr):
+    total = len(av_cell_nrs)
     probs = ""
     for i in range(total):
-        probs += "1/{}::pos({},{})".format(total,str(i),turn_nr)
+        probs += "1/{}::pos({},{})".format(total,av_cell_nrs[i],turn_nr)
         if i < total - 1: 
             probs += "; "
     return probs + ".\n"
 
+# Uniform distribution containing all available moves
+def stringForAllMoves(state, turn_nr):
+    cells = louiswork.available_cells(state)
+    return stringOfProbDist(map(+1, cells), turn_nr)
+
 # Get adjacent cells on grid (hardcoded for 3x3)
-def adjacentCells(cell):
-    if not (cell in range(9)):
+def adjacentCells(cell_nr):
+    if not (cell_nr in range(9)):
+        print("Off the grid!")
         return []
     
     coord_dict = {
         "1" : (1,1),
-        "2" : (1,2),
-        "3" : (1,3),
-        "4" : (2,1),
+        "2" : (2,1),
+        "3" : (3,1),
+        "4" : (1,2),
         "5" : (2,2), 
-        "6" : (2,3),
-        "7" : (3,1),
-        "8" : (3,2),
+        "6" : (3,2),
+        "7" : (1,3),
+        "8" : (2,3),
         "9" : (3,3)
     } 
     
-    (i,j) = coord_dict[cell]
-    adj_coords = [(i-1,j), (i,j-1), (i+1,j), (i,j+1)]
-    adj_cells = [i for i,c in coord_dict.iteritems() if c in adj_coords]
+    (i,j) = coord_dict[str(cell_nr)]
+
+    # 5 is middle cell
+    if cell_nr == 5:
+        adj_coords = range(1,9)
+    else:
+        adj_coords = [(i-1,j), (i,j-1), (i+1,j), (i,j+1),
+                        (i-1,j-1), (i-1,j+1), (i+1,j-1), (i+1,j+1)]
+    adj_cells = [int(i) for i,c in coord_dict.items() if c in adj_coords]
 
     return adj_cells
 
 
 # Try winning as fast as possible by choosing adjacent tiles
 def stringForWinningFast(state, turn_nr): 
+
+    # If grid is empty, select all possible moves
+    if state == (None,) * 9: 
+        return stringForAllMoves(range(1,9), turn_nr)
+
     cells = louiswork.available_cells(state)
+    chosen_cells = []
+
+    # Collect cells adjacent to multiple cells with an "x"
     for cell in cells:
-        adj_cells = adjacentCells(cell)
-        if any()
+        adj_cells = [c for c in adjacentCells(cell) if c == "x"]
+        print(adj_cells)
+        if len(adj_cells) > 1: 
+            chosen_cells += cell 
+    
+    print("chosen: ", chosen_cells)
+
+    # If no such cells are available, select all possible moves
+    if chosen_cells == []: 
+        return stringForAllMoves(range(1,9), turn_nr)
+
+    return stringOfProbDist(chosen_cells, turn_nr)
 
 
 # Input max turn number in which you want to win
@@ -55,8 +84,15 @@ def stringForMPE(max_turn):
 
 
 
+nice_state = ("x", "o", "x", None, None, None, "x", None, None)
+nice_cell_nrs = louiswork.available_cells(nice_state)
 
-
-print(stringForUniDist((None,"x",None,"o"),2))
+print("propbdist: ", stringOfProbDist(nice_cell_nrs,2))
+print(louiswork.available_cells(nice_state))
+print("probdist of all: ", stringForAllMoves(nice_state,2))
+print("next to 5: ", adjacentCells(5))
+print("illegal: ", adjacentCells(11))
+print("winning fast: ", stringForWinningFast(nice_state, 3))
+print("evidence str: ",stringForMPE(3))
 
     
