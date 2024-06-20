d93237d8fc7983d222b0c11fa85bd78d84bf6254
Madeleine Gignoux <madeleine.gignoux@gmail.com>
2024-06-20 16:01:01 +0200

    Update to snake case

--- simulator.py
+++ simulator.py
@@ -3,22 +3,22 @@ import random
 # "we" always play "x"
 # "computer" always plays "o"
 
-def playGame(move, grid, state):
+def play_game(move, grid, state):
     if move == "e":
         move = random.choice(("x","o"))
     while louiswork.winner(state) == None:
         if move == "x":
             (_, cell), (_, _) = louiswork.value(grid, state) # input the strategy we use here
-            state = makeMove("x","o",state,cell,grid)
+            state = make_move("x","o",state,cell,grid)
             move == "o"
         elif move == "o":
             (_, _), (_, cell) = louiswork.value(grid, state)
-            state = makeMove("o","x",state,cell,grid)
+            state = make_move("o","x",state,cell,grid)
             move == "x"
     else:
         return louiswork.winner(state)
     
-def makeMove(player,opponent,state,cell,grid):
+def make_move(player,opponent,state,cell,grid):
     success, neutral, failure = grid[cell]
     choice = random.choice(range(1,100))/100
     if choice <= success:
@@ -32,8 +32,8 @@ def simulate(player):
     gameswonx = 0
     gameswono = 0
     gamestied = 0 
-    for _ in range(100000):
-        result = playGame(player, louiswork.grid,(None,) * 9)
+    for _ in range(1000):
+        result = play_game(player, louiswork.grid,(None,) * 9)
         if result == "x":
             gameswonx += 1
         elif result == "o":
