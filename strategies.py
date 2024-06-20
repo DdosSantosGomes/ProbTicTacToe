d36dd04c95655ccfdaf8c617fcc1d0d88b9c2ebc
DdosSantosGomes <djanira.westra@gmail.com>
2024-06-20 15:29:38 +0200

    Made separate file for helper functions, finished Winning Fast strategy

--- strategies.py
+++ strategies.py
@@ -1,54 +1,11 @@
 import louiswork
 import simulator
 import game_logic
+import strategy_helper
 
+# Example (init game): uniform dist of all tiles 
 # 1/9::pos(1,1); 1/9::pos(2,1); 1/9::pos(3,1); 1/9::pos(4,1); 1/9::pos(5,1); 
 #                1/9::pos(6,1); 1/9::pos(7,1); 1/9::pos(8,1); 1/9::pos(9,1).
-
-# Returns Problog-style string of unif. prob. dist. given list of available cell nrs
-def stringOfProbDist(av_cell_nrs, turn_nr):
-    total = len(av_cell_nrs)
-    probs = ""
-    for i in range(total):
-        probs += "1/{}::pos({},{})".format(total,av_cell_nrs[i],turn_nr)
-        if i < total - 1: 
-            probs += "; "
-    return probs + ".\n"
-
-# Uniform distribution containing all available moves
-def stringForAllMoves(state, turn_nr):
-    cells = louiswork.available_cells(state)
-    return stringOfProbDist(map(+1, cells), turn_nr)
-
-# Get adjacent cells on grid (hardcoded for 3x3)
-def adjacentCells(cell_nr):
-    if not (cell_nr in range(9)):
-        print("Off the grid!")
-        return []
-    
-    coord_dict = {
-        "1" : (1,1),
-        "2" : (2,1),
-        "3" : (3,1),
-        "4" : (1,2),
-        "5" : (2,2), 
-        "6" : (3,2),
-        "7" : (1,3),
-        "8" : (2,3),
-        "9" : (3,3)
-    } 
-    
-    (i,j) = coord_dict[str(cell_nr)]
-
-    # 5 is middle cell
-    if cell_nr == 5:
-        adj_coords = range(1,9)
-    else:
-        adj_coords = [(i-1,j), (i,j-1), (i+1,j), (i,j+1),
-                        (i-1,j-1), (i-1,j+1), (i+1,j-1), (i+1,j+1)]
-    adj_cells = [int(i) for i,c in coord_dict.items() if c in adj_coords]
-
-    return adj_cells
 
 
 # Try winning as fast as possible by choosing adjacent tiles
@@ -56,25 +13,26 @@ def stringForWinningFast(state, turn_nr):
 
     # If grid is empty, select all possible moves
     if state == (None,) * 9: 
-        return stringForAllMoves(range(1,9), turn_nr)
+        return strategy_helper.stringForAllMoves([*range(1,10)], turn_nr)
 
-    cells = louiswork.available_cells(state)
+    cells = [c + 1 for c in louiswork.available_cells(state)]
+    print("cells: ", cells)
     chosen_cells = []
 
-    # Collect cells adjacent to multiple cells with an "x"
+    # Collect only cells adjacent to multiple cells with an "x"
     for cell in cells:
-        adj_cells = [c for c in adjacentCells(cell) if c == "x"]
-        print(adj_cells)
+        adj_cells = [c for c in strategy_helper.adjacentCells(cell) if state[c-1] == "x"]
+        print("next to {}:".format(str(cell)), adj_cells)
         if len(adj_cells) > 1: 
-            chosen_cells += cell 
+            chosen_cells.append(cell)
     
     print("chosen: ", chosen_cells)
 
-    # If no such cells are available, select all possible moves
+    # If no such cells are available, default to all possible moves
     if chosen_cells == []: 
-        return stringForAllMoves(range(1,9), turn_nr)
+        return strategy_helper.stringForAllMoves([*range(1,10)], turn_nr)
 
-    return stringOfProbDist(chosen_cells, turn_nr)
+    return strategy_helper.stringOfProbDist(chosen_cells, turn_nr)
 
 
 # Input max turn number in which you want to win
@@ -82,17 +40,16 @@ def stringForWinningFast(state, turn_nr):
 def stringForMPE(max_turn):
     return "evidence(win({}))".format(max_turn)
 
-
-
+# Some tests
 nice_state = ("x", "o", "x", None, None, None, "x", None, None)
-nice_cell_nrs = louiswork.available_cells(nice_state)
+nice_cell_nrs = [4,5,6,8,9]
 
-print("propbdist: ", stringOfProbDist(nice_cell_nrs,2))
-print(louiswork.available_cells(nice_state))
-print("probdist of all: ", stringForAllMoves(nice_state,2))
-print("next to 5: ", adjacentCells(5))
-print("illegal: ", adjacentCells(11))
-print("winning fast: ", stringForWinningFast(nice_state, 3))
-print("evidence str: ",stringForMPE(3))
+# print("probdist: ", stringOfProbDist(nice_cell_nrs,2))
+# print("probdist of all (should be same): ", stringForAllMoves(nice_state,2))
+# print("next to 6: ", adjacentCells(6))
+# print("next to 5: ", adjacentCells(5))
+# print("illegal: ", adjacentCells(11))
+# print("winning fast: ", stringForWinningFast(nice_state, 3))
+# print("evidence str: ",stringForMPE(3))
 
     
