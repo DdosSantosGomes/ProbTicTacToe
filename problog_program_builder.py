7a0a254fd3056dbf099eabe04af2f803599b62ed
nanochanuwu <nano.batistoni@student.uva.nl>
2024-06-23 15:37:40 +0200

    added update functions + changed double underscores into single

--- problog_program_builder.py
+++ problog_program_builder.py
@@ -10,10 +10,10 @@ from names import *
 
 class ProbLogProgram:
     """
-    Objects of this class are ProbLog programs that model a game of ProbTicTacToe.
+    Objects of this class are ProbLog programs that model a state of the game of ProbTicTacToe.
     """
 
-    __program = {}
+    _program = {}
     """
     This is an internal representation which should be kept hidden. \n
     It is a dictionary indexed by the sections of our ProbLog program.
@@ -24,44 +24,44 @@ class ProbLogProgram:
         - moves (legal move predicate)
         - win (winning condition)
         - lose (losing condition)
-        - choices (probability distribution of square choices)
+        - play (uniform probability distribution of the candidate cells to play, fed by the strategies)
         - (possibly) queries
         - (possibly) evidence \n
     and they correspond to the fixed game logic of the game of ProbTicTacToe.
     """
     
     def __init__(self, grid): 
-        self.grid = self.__generate_grid() if grid is None else grid
-        self.__init_board()
-        self.__grid()
-        self.__init_turns()
-        self.__moves()
-        self.__win_condition()
-        self.__lose_condition()
+        self.grid = self._generate_grid() if grid is None else grid
+        self._init_board()
+        self._grid()
+        self._init_turns()
+        self._moves()
+        self._win_condition()
+        self._lose_condition()
 
     def get_program(self):
         program = ''
-        for index,prog in self.__program.items():
+        for index,prog in self._program.items():
             if index == 'queries' or index == 'evidence':
                 continue
             program += prog
         # make sure that we add evidence and then queries at the end of the program
-        if 'evidence' in self.__program.keys():
-            program += self.__program['evidence']
-        if 'queries' in self.__program.keys():
-            program += self.__program['queries']
+        if 'evidence' in self._program.keys():
+            program += self._program['evidence']
+        if 'queries' in self._program.keys():
+            program += self._program['queries']
         return program
 
     def query(self, *queries, evidence = None):
         """
         Use in combination with utils.query and utils.evidence to generate syntactically valid ProbLog queries.
         """
-        self.__program['queries'] = ''
+        self._program['queries'] = ''
         for query in queries:
-            self.__program['queries'] += query
+            self._program['queries'] += query
         try:
             if evidence is not None:
-                self.__program['evidence'] = evidence
+                self._program['evidence'] = evidence
             problog_program = PrologString(self.get_program())
             result = get_evaluatable().create_from(problog_program).evaluate()
         except:
@@ -71,13 +71,21 @@ class ProbLogProgram:
         return result
     
     def update_board(self, new_board):
-        self.__program['board'] = new_board
+        self._program['board'] = new_board
 
-    def update_choices(self, choices):
-        self.__program['choices'] = choices
+    def update_win_conditions(self, *conditions):
+        for condition in conditions: 
+            self._program['win'] += condition
+
+    def update_lose_conditions(self, *conditions):
+        for condition in conditions: 
+            self._program['lose'] += condition
+
+    def update_play(self, prob_dist):
+        self._program['play'] = prob_dist
     
-    def __grid(self):
-        self.__program['grid'] = ''
+    def _grid(self):
+        self._program['grid'] = ''
         for cell_no in range(len(self.grid)):
             current_cell = self.grid[cell_no]
             p_good, p_neutral, p_bad = current_cell[0], current_cell[1], current_cell[2]
@@ -90,28 +98,28 @@ class ProbLogProgram:
             a3 = probabilistic_fact(p_bad, 
                                     function(SQUARE_BAD, constant(cell_no+1), variable(A))
                                     )
-            self.__program['grid'] += annotated_disjunction_with_body( 
+            self._program['grid'] += annotated_disjunction_with_body( 
                 annotated_disj = annotated_disjunction(a1, a2, a3),
                 body = function(TURN, ANY, variable(A))
             )
 
-    def __init_turns(self):
-        self.__program['turns'] = ''
+    def _init_turns(self):
+        self._program['turns'] = ''
         for turn_no in range(1, 4): # we only look 3 turns ahead
             player = "x" if turn_no % 2 == 1 else "o"
-            self.__program['turns'] += fact(function(TURN, constant(player), constant(turn_no)))
+            self._program['turns'] += fact(function(TURN, constant(player), constant(turn_no)))
 
-    def __init_board(self):
+    def _init_board(self):
         initial_config = (N,) * (len(self.grid)) + (0,)
-        self.__program['start_board'] = fact(
+        self._program['start_board'] = fact(
             function(
                 BOARD, 
                 *[ constant(x) for x in initial_config ]
                 )
             )
 
-    def __moves(self):
-        self.__program['moves'] = ''
+    def _moves(self):
+        self._program['moves'] = ''
         for cell_no in range(1, len(self.grid) + 1):
             before = ['S' + str(j) for j in range(1, cell_no)]
             after = ['S' + str(k) for k in range(cell_no+1, len(self.grid)+1)]
@@ -175,16 +183,16 @@ class ProbLogProgram:
                 ),
                 constraint = simple_constraint(variable(B), variable(A))
             )
-            self.__program['moves'] = self.__program['moves'] + x_good + o_good + x_bad + o_bad + neutral
+            self._program['moves'] = self._program['moves'] + x_good + o_good + x_bad + o_bad + neutral
 
-    def __win_condition(self): # only defined for grid sizes up to 3, since this is what we are gonna use anyway
+    def _win_condition(self): # only defined for grid sizes up to 3, since this is what we are gonna use anyway
         size = int(sqrt(len(self.grid)))
         if size == 1:
-            self.__program['win'] = "win(B) :- board(x,B).\n\n"
+            self._program['win'] = "win(B) :- board(x,B).\n\n"
         elif size == 2: # exclude diagonals for a more fair game
-            self.__program['win'] = "win(B) :- board(x,x,_,_,B); board(x,_,x,_,B); board(_,x,_,x,B).\n\n"
+            self._program['win'] = "win(B) :- board(x,x,_,_,B); board(x,_,x,_,B); board(_,x,_,x,B).\n\n"
         elif size == 3:
-            self.__program['win'] = ("win1(B) :- board(x,x,x,S4,S5,S6,S7,S8,S9,B).\n"
+            self._program['win'] = ("win1(B) :- board(x,x,x,S4,S5,S6,S7,S8,S9,B).\n"
             + "win1(B) :- win1(A), B is A+1.\n"
             + "win2(B) :- board(S1,S2,S3,x,x,x,S7,S8,S9,B).\n"
             + "win2(B) :- win2(A), B is A+1.\n"
@@ -204,14 +212,14 @@ class ProbLogProgram:
         else:
             raise ConditionUndefinedException("Win condition undefined for grid sizes > 3. Sorry!")
 
-    def __lose_condition(self): # only defined for grid sizes up to 3, since this is what we are gonna use anyway
+    def _lose_condition(self): # only defined for grid sizes up to 3, since this is what we are gonna use anyway
         size = int(sqrt(len(self.grid)))
         if size == 1:
-            self.__program['lose'] = "lose(B) :- board(o,B).\n\n"
+            self._program['lose'] = "lose(B) :- board(o,B).\n\n"
         elif size == 2:
-            self.__program['lose'] = "lose(B) :- board(o,o,_,_,B); board(o,_,o,_,B); board(_,o,_,o,B).\n\n"
+            self._program['lose'] = "lose(B) :- board(o,o,_,_,B); board(o,_,o,_,B); board(_,o,_,o,B).\n\n"
         elif size == 3:
-            self.__program['lose'] = ("lose1(B) :- board(o,o,o,S4,S5,S6,S7,S8,S9,B).\n"
+            self._program['lose'] = ("lose1(B) :- board(o,o,o,S4,S5,S6,S7,S8,S9,B).\n"
             + "lose1(B) :- lose1(A), B is A+1.\n"
             + "lose2(B) :- board(S1,S2,S3,o,o,o,S7,S8,S9,B).\n"
             + "lose2(B) :- lose2(A), B is A+1.\n"
@@ -232,7 +240,7 @@ class ProbLogProgram:
             raise ConditionUndefinedException("Lose condition undefined for grid sizes > 3. Sorry!")
         
     # only for testing. Based on Louis Abraham's work
-    def __generate_grid(self):
+    def _generate_grid(self):
         def generate_square():
             neutral = random.choice(range(5, 35, 5))
             success = random.choice(range(30, 100 - neutral + 5, 5))
